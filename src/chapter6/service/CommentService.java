a01cc37ae1af1abf7a775276bba9abe2058230fc
usui_riku <usui.riku@alhinc.jp>
2026-03-18 14:44:11 +0900

    feat:つぶやきに返信する機能を追加

--- src/chapter6/service/CommentService.java
+++ src/chapter6/service/CommentService.java
@@ -8,7 +8,9 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import chapter6.beans.Comment;
 import chapter6.beans.UserComment;
+import chapter6.dao.CommentDao;
 import chapter6.dao.UserCommentDao;
 import chapter6.logging.InitApplication;
 
@@ -25,6 +27,34 @@ public class CommentService {
 	public CommentService() {
 		InitApplication application = InitApplication.getInstance();
 		application.init();
+	}
+
+	public void insert(Comment comment) {
+		log.info(new Object() {
+		}.getClass().getEnclosingClass().getName() + " : " + new Object() {
+		}.getClass().getEnclosingMethod().getName());
+
+		Connection connection = null;
+		try {
+			connection = getConnection();
+			new CommentDao().insert(connection, comment);
+
+			commit(connection);
+		} catch (RuntimeException e) {
+			rollback(connection);
+			log.log(Level.SEVERE, new Object() {
+			}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
+
+			throw e;
+		} catch (Error e) {
+			rollback(connection);
+			log.log(Level.SEVERE, new Object() {
+			}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
+
+			throw e;
+		} finally {
+			close(connection);
+		}
 	}
 
 	public List<UserComment> select() {
